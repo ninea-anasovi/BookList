f1db97226cceece5a781e3af9fc6cc1f63533a1c
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-08-12 22:11:42 +0400

    created react app

---
+++ booklist/src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../HeroSection'
+import SearchBar from '../Searchbar'
+
+function Layout() {
+  return (
+    <>
+        <HeroSection/>
+    </>
+  )
+}
+
+export default Layout
