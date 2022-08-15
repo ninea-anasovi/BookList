89a1edb76d10ca2bd0d3af2e2cc34f6324de71ec
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-08-15 16:28:27 +0400

    mobile edit

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -5,7 +5,7 @@ import SearchBar from '../Searchbar'
 
 function HeroSection() {
   return (
-    <section className="px-40 py-40 bg-[url('Assets/images/hero-image.jpg')] bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
+    <section className="px-10 py-40 bg-[url('Assets/images/hero-image.jpg')] bg-hero bg-no-repeat bg-cover bg-center bg-fixed lg:px-40">
       <h1 className="max-w-2xl mb-8 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
         Book Shelf For Book Lovers to Enjoy
       </h1>
--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -31,7 +31,7 @@ function SearchBar() {
     <>
         <form >   
             <label htmlFor="default-search" className=" mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-            <div className="relative">
+            <div className="relative focus:none">
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none ">
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
@@ -40,7 +40,7 @@ function SearchBar() {
                   value={search} 
                   onChange={e=>setSearch(e.target.value)} 
                   onKeyPress={searchBook}  
-                  className="block pl-10 w-full  text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  py-6" 
+                  className="block pl-10 w-full  text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:none py-6 " 
                   placeholder="Search Books..."
                   required/>
                 <button 
